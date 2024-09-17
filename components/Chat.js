af98a66048e33d31eb2cfaa4b195cd6efa0f8e5d
mtaut <146488331+mtaut@users.noreply.github.com>
2024-09-17 16:23:24 -0500

    ex5.2 added chat ui/gifted-chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,78 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 
 // this is the chat screen
 const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
+
+  // setting name and bg color of UI
   const { name, backgroundColor } = route.params;
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+
+    // default messages sent in chat
+    setMessages([
+      {
+        _id: 1,
+        text: "You have entered a Chatty",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+      {
+        _id: 2,
+        text: "This is a system message",
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
+
+  // function to send messages
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  // text bubble color function
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+          left: {
+            backgroundColor: "#FFF",
+          },
+        }}
+      />
+    );
+  };
 
   return (
     <View style={[styles.container, { backgroundColor: backgroundColor }]}>
-      <Text style={styles.welcomeText}>Welcome to Chatties!</Text>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? -220 : 0}
+      >
+        <GiftedChat
+          messages={messages}
+          renderBubble={renderBubble}
+          onSend={(messages) => onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+      </KeyboardAvoidingView>
     </View>
   );
 };
@@ -19,11 +80,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  welcomeText: {
-    color: "#FFFFFF",
   },
 });
 
